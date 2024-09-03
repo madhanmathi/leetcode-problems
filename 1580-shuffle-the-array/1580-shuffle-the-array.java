54647113d1add7ce41edb58a5edd5bad887aaeca
MATHAN T <138795355+madhanmathi@users.noreply.github.com>
2024-09-04 00:48:00 +0530

    Time: 0 ms (100%), Space: 44.7 MB (26.94%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.java
+++ 1580-shuffle-the-array/1580-shuffle-the-array.java
@@ -1,23 +1,11 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        
-   return func(nums,n); }
-    static int[] func(int[] nums,int n){
+        int[] res=new int[nums.length];
         int idx=0;
-        int[] array1=new int[n];
-        int[] array2=new int[n];
-        
-             for(int i=0;i<n;i++){
-             array1[i]=nums[i];
-}
-for(int j=n;j<nums.length;j++){
-     array2[j-n]=nums[j];
-    }
-    for(int i=0;i<n;i++){
-        nums[idx++]=array1[i];
-        nums[idx++]=array2[i];
-
-    }
-    return nums;
-}
+        for(int i=0;i<n;i++){
+            res[idx++]=nums[i];
+            res[idx++]=nums[i+n];
+        }
+        return res;
+        }
 }
