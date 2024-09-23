d895dc60673346767229fd2b4e7219afe1beb83b
MATHAN T <138795355+madhanmathi@users.noreply.github.com>
2024-09-23 22:13:06 +0530

    Time: 0 ms (100%), Space: 41.7 MB (82.71%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,9 @@
+class Solution {
+    public double myPow(double x, int n) {
+        return func(x,n);
+    }
+     static double  func(double  a,int b){
+                Double arr=Math.pow(a, b);
+               return arr;
+       }
+}
