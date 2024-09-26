41c80478deca1cc2274fe58074ed6a446ec3cd4e
MATHAN T <138795355+madhanmathi@users.noreply.github.com>
2024-09-26 22:51:54 +0530

    Time: 1 ms (98.8%), Space: 43.7 MB (39.44%) - LeetHub

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] finalPrices(int[] arr) {
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+             if(arr[i]>=arr[j]){
+                arr[i]=arr[i]-arr[j];
+                break;
+             }
+            }
+          }
+          return arr;
+    }
+}
