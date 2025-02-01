b1004de361547aed27982969307878c55271682c
MATHAN T <138795355+madhanmathi@users.noreply.github.com>
2025-02-01 21:43:54 +0530

    Time: 1 ms (37.58%), Space: 43.6 MB (32.6%) - LeetHub

---
+++ 3429-special-array-i/3429-special-array-i.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isArraySpecial(int[] arr) {
+        if(arr.length==1) return true;
+        for(int i=0;i<arr.length-1;i++){
+            if((arr[i]%2==0)==(arr[i+1]%2==0)) return false;
+             if((arr[i]%2!=0)==(arr[i+1]%2!=0)) return false;
+        }
+        return true;
+    }
+}
