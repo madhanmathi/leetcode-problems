94a1bd83e18ae4dd3ea8413c9ec8981119d1dc36
MATHAN T <138795355+madhanmathi@users.noreply.github.com>
2025-02-16 23:06:30 +0530

    Time: 5 ms (47.34%), Space: 44.7 MB (41.52%) - LeetHub

--- 2283-sort-even-and-odd-indices-independently/2283-sort-even-and-odd-indices-independently.java
+++ 2283-sort-even-and-odd-indices-independently/2283-sort-even-and-odd-indices-independently.java
@@ -1,31 +1,22 @@
 class Solution {
     public int[] sortEvenOdd(int[] nums) {
-        if(nums.length<3)
-        return nums;
-        List<Integer> odd=new ArrayList<>();
-        List<Integer> even=new ArrayList<>();
-        int ans[]=new int[nums.length];
-        for(int i=0;i<nums.length;i++)
-        {
-            if (i%2 == 0) 
-                even.add(nums[i]);
-                else 
-                odd.add(nums[i]);
-            }
-        
-        Collections.sort(even);
-        Collections.sort(odd);
-        Collections.reverse(odd);
-		
-        int j=0;
-        int k=0;
-        for(int i=0;i<nums.length;i++)
-        {
-             if (i%2 == 0) 
-               ans[i]=even.get(j++);
-             else 
-               ans[i]=odd.get(k++);
-        }
-        return ans;
+        if(nums.length<=2) return nums;
+        List<Integer> list1=new ArrayList<>();
+         List<Integer> list2=new ArrayList<>();
+         int[] ans=new int[nums.length];
+         for(int i=0;i<nums.length;i++){
+            if(i%2==0) list1.add(nums[i]);
+            else list2.add(nums[i]);
+         }
+         Collections.sort(list1);
+          Collections.sort(list2);
+          Collections.reverse(list2);
+          int j=0;
+          int k=0;
+          for(int i=0;i<ans.length;i++){
+            if(i%2==0) ans[i]=list1.get(j++);
+            else ans[i]=list2.get(k++);
+          }
+          return ans;
     }
 }
