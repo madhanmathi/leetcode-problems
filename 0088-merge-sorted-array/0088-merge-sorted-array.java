14e75f73549fb97a885e4035441824e99e67b54a
MATHAN T <138795355+madhanmathi@users.noreply.github.com>
2025-03-04 23:14:58 +0530

    Time: 0 ms (100%), Space: 42.3 MB (41.68%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,17 +1,21 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-                List<Integer> list=new ArrayList<>();
-        for(int i=0;i<m;i++){
-            list.add(nums1[i]);
-        }
-        for(int j=0;j<n;j++){
-            list.add(nums2[j]);
-        }
-        int[] arr=new int[list.size()];
-        for(int i=0;i<arr.length;i++){
-            nums1[i]=list.get(i);
-        }
-        Arrays.sort(nums1);
-        System.out.print(Arrays.toString(nums1));
+              int n1=0;
+              int n2=0;
+            int[] res=new int[m+n];
+            int idx=0;
+            while(n1<m&&n2<n){
+                if(nums1[n1]<nums2[n2]) res[idx++]=nums1[n1++];
+                else if(nums1[n1]>nums2[n2]) res[idx++]=nums2[n2++];
+                else{
+                    res[idx++]=nums1[n1++];
+                      res[idx++]=nums2[n2++]; 
+                }
+            }
+            while(n1<m) res[idx++]=nums1[n1++];
+             while(n2<n) res[idx++]=nums2[n2++];
+             for(int i=0;i<res.length;i++){
+                nums1[i]=res[i];
+             }
     }
 }
