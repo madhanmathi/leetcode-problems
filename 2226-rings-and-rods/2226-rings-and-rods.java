1b84af3f46d4becfd66456ae8af43a9f5e161788
MATHAN T <138795355+madhanmathi@users.noreply.github.com>
2025-03-14 22:33:31 +0530

    Time: 0 ms (100%), Space: 41.6 MB (51%) - LeetHub

--- 2226-rings-and-rods/2226-rings-and-rods.java
+++ 2226-rings-and-rods/2226-rings-and-rods.java
@@ -1,15 +1,19 @@
 class Solution {
     public int countPoints(String s) {
-        int count=0;
-       for(int i=0;i<10;i++){
-         String a="";
-        for(int j=1;j<s.length();j+=2){
-            char ch=s.charAt(j-1);
-            char ch2=s.charAt(j);
-           if((!a.contains(String.valueOf(ch)))&&(ch2-'0')==i)  a=a+ch;
+        int[] r=new int[10];
+        int[] g=new int[10];
+        int[] b=new int[10];
+        for(int i=0;i<s.length();i+=2){
+            char ch=s.charAt(i);
+            char ch2=s.charAt(i+1);
+            if(ch=='R') r[ch2-'0']++;
+            else if(ch=='G') g[ch2-'0']++;
+            else if(ch=='B') b[ch2-'0']++;
         }
-        if(a.length()==3) count++;
-       }
-       return count;
+        int count=0;
+        for(int i=0;i<10;i++){
+            if(r[i]>0&&g[i]>0&&b[i]>0) count++;
+        }
+        return count;
     }
 }
