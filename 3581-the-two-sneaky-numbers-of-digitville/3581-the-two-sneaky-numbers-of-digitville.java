cf373d32fb1b76366e5ff2bfc7cbe78e45b2f2ef
MATHAN T <138795355+madhanmathi@users.noreply.github.com>
2025-03-27 16:07:09 +0530

    Time: 1 ms (100%), Space: 45.1 MB (15.83%) - LeetHub

--- 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.java
@@ -1,14 +1,14 @@
 class Solution {
     public int[] getSneakyNumbers(int[] arr) {
-         Arrays.sort(arr);
         int[] ans=new int[2];
-        int k=0;
-        for(int i=0;i<arr.length-1;i++){
-            if(arr[i]==arr[i+1]){
-               ans[k]=arr[i];
-               k++;
-            }
-        }
+       int k=0;
+       int[] freq=new int[100];
+       for(int i=0;i<arr.length;i++){
+        freq[arr[i]]++;
+       }
+       for(int i=0;i<100;i++){
+        if(freq[i]==2) ans[k++]=i;
+       }
         return ans;
     }
 }
