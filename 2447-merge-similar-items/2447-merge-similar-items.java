6c12502a4f0904a6c34d2a113c01d8252cb0051c
MATHAN T <138795355+madhanmathi@users.noreply.github.com>
2025-04-06 22:40:48 +0530

    Time: 8 ms (79.43%), Space: 46.2 MB (9.85%) - LeetHub

---
+++ 2447-merge-similar-items/2447-merge-similar-items.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> mergeSimilarItems(int[][] num1, int[][] num2) {
+       List<List<Integer>> res=new ArrayList<>(); 
+          HashMap<Integer,Integer> map=new HashMap<>();
+          int min1=Integer.MAX_VALUE;
+          int max1=Integer.MIN_VALUE;
+          for(int i=0;i<num1.length;i++){
+            map.put(num1[i][0],num1[i][1]);
+            min1=Math.min(min1,num1[i][0]);
+            max1=Math.max(max1,num1[i][0]);
+          } 
+      for(int i=0;i<num2.length;i++){
+         min1=Math.min(min1,num2[i][0]);
+            max1=Math.max(max1,num2[i][0]);
+        int val=map.getOrDefault(num2[i][0],0);
+          map.put(num2[i][0],val+num2[i][1]);
+      }
+    for(int i=1;i<=max1;i++){
+        if(map.containsKey(i)){
+            List<Integer> list=new ArrayList<>();
+            list.add(i);
+            list.add(map.get(i));
+           res.add(list);
+        }
+       
+    }
+    return res;
+    }
+}
