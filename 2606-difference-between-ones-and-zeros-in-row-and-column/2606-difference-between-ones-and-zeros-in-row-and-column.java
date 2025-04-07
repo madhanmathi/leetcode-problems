ee3e61a8cd0098b155f5c2758c40917b5fb4a686
MATHAN T <138795355+madhanmathi@users.noreply.github.com>
2025-04-07 22:38:51 +0530

    Time: 25 ms (6.97%), Space: 77.9 MB (88.2%) - LeetHub

---
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/2606-difference-between-ones-and-zeros-in-row-and-column.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[][] onesMinusZeros(int[][] grid) {
+        int[] arr1=new int[grid.length];
+        int[] arr2=new int[grid[0].length];
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[i].length;j++){
+             arr1[i]=arr1[i]+grid[i][j];
+            }
+        }
+         for(int i=0;i<grid[0].length;i++){
+            for(int j=0;j<grid.length;j++){
+             arr2[i]=arr2[i]+grid[j][i];
+            }
+        }
+        System.out.print(Arrays.toString(arr1));
+         System.out.print(Arrays.toString(arr2));
+        int n=arr1.length;
+        int m=arr2.length;
+       for(int i=0;i<grid.length;i++){
+        for(int j=0;j<grid[0].length;j++){
+          grid[i][j]=arr1[i]+arr2[j]-(m-arr1[i])-(n-arr2[j]);
+        }
+       }
+       return grid;
+    }
+}
