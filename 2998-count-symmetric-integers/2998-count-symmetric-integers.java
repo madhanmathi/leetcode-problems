cf6ad3b3dffd4e6af894506697174be1258bb85d
MATHAN T <138795355+madhanmathi@users.noreply.github.com>
2025-04-15 22:36:14 +0530

    Time: 32 ms (23.96%), Space: 45 MB (9.71%) - LeetHub

--- 2998-count-symmetric-integers/2998-count-symmetric-integers.java
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.java
@@ -6,14 +6,8 @@ class Solution {
              int sum1=0;
               int sum2=0;
             String a=String.valueOf(i);
-          //  if(a.length()%2==0){
-                for(int j=0;j<a.length()/2;j++){
-                   sum1=sum1+Integer.valueOf(a.charAt(j));
-                }
-                for(int k=a.length()/2;k<a.length();k++){
-                   sum2=sum2+Integer.valueOf(a.charAt(k));
-                }
-        //    }
+         for(int j=0;j<a.length()/2;j++) sum1=sum1+Integer.valueOf(a.charAt(j));
+         for(int k=a.length()/2;k<a.length();k++) sum2=sum2+Integer.valueOf(a.charAt(k));
             if(sum1==sum2) count++;
        }
         return count;
