eca81341ba001098f04e778734b3808cd58b5807
MATHAN T <138795355+madhanmathi@users.noreply.github.com>
2025-04-21 15:44:54 +0530

    Time: 51 ms (22.12%), Space: 45.7 MB (25.56%) - LeetHub

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/1469-minimum-number-of-steps-to-make-two-strings-anagram.java
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/1469-minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -1,22 +1,18 @@
 class Solution {
     public int minSteps(String s, String t) {
-      Map<String,Integer> map1=new HashMap<>();
-      Map<String,Integer> map2=new HashMap<>();
-      String[] arr1=s.split("");
-      String[] arr2=t.split("");
-      for(String i:arr1){
+      Map<Character,Integer> map1=new HashMap<>();
+      Map<Character,Integer> map2=new HashMap<>();
+      for(char i:s.toCharArray()){
         map1.put(i,map1.getOrDefault(i,0)+1);
       }
-       for(String i:arr2){
+       for(char i:t.toCharArray()){
         map2.put(i,map2.getOrDefault(i,0)+1);
       }
       int count=0;
       int val=0;
-      for(String i:map1.keySet()){
-        if((map2.keySet()).contains(i)&&map1.get(i)>map2.get(i)){
-            count=count+(map1.get(i)-map2.get(i));
-        }
-        if(!(map2.keySet()).contains(i)) val=val+map1.get(i);
+      for(char i:map1.keySet()){
+    if(map2.containsKey(i)&&map1.get(i)>map2.get(i)) count=count+(map1.get(i)-map2.get(i));
+        if(!map2.containsKey(i)) val=val+map1.get(i);
       }
       return count+val;
     }
