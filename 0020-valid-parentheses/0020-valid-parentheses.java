9e071eac8126cac4520e4fb935f785d771d55ff9
MATHAN T <138795355+madhanmathi@users.noreply.github.com>
2025-06-20 16:55:51 +0530

    Time: 45 ms (5.43%), Space: 45.3 MB (5.71%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,19 +1,11 @@
 class Solution {
     public boolean isValid(String s) {
-     Stack<Character> stack=new Stack<>();
-     HashMap<Character,Character> map=new HashMap<>();
-     map.put(')','('); 
-     map.put(']','[');   
-     map.put('}','{');     
-     if(s.length()<=1) return false;
-     for(int i=0;i<s.length();i++){
-        if(s.charAt(i)=='('||s.charAt(i)=='['||s.charAt(i)=='{') stack.push(s.charAt(i));
-        else if(stack.isEmpty() || map.get(s.charAt(i))!=stack.peek()) return false;
-        else{
-            stack.pop();
+        while(s.length()!=0){
+            if(s.contains("()")) s=s.replace("()","");
+            else if(s.contains("[]")) s=s.replace("[]","");
+              else if(s.contains("{}")) s=s.replace("{}","");
+               else return false;
         }
-     }  
-     if(stack.isEmpty()) return true;
-     return false; 
+        return true;
     }
 }
