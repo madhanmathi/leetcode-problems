24c14bd5f28aef3189620cd6d53265b0794c2bf5
MATHAN T <138795355+madhanmathi@users.noreply.github.com>
2025-09-07 18:14:18 +0530

    Time: 505 ms (92.88%), Space: 0B (100%) - LeetHub

--- 2110-employees-with-missing-information/2110-employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/2110-employees-with-missing-information.sql
@@ -1,10 +1,25 @@
-with employee_common as(
-    select e.employee_id emp_id,s.employee_id sal_id from employees e left join salaries s on e.employee_id=s.employee_id
-)
-,salary_common as(
-    select s.employee_id sal_id,e.employee_id emp_id from employees e right join salaries s on e.employee_id=s.employee_id 
-)
-select emp_id employee_id from employee_common where sal_id is null
-union
-select sal_id from salary_common where emp_id is null order by
-employee_id;
+SELECT 
+  employee_id 
+FROM 
+  Employees 
+WHERE 
+  employee_id NOT IN (
+    SELECT 
+      employee_id 
+    FROM 
+      Salaries
+  ) 
+UNION 
+SELECT 
+  employee_id 
+FROM 
+  Salaries 
+WHERE 
+  employee_id NOT IN (
+    SELECT 
+      employee_id 
+    FROM 
+      Employees
+  ) 
+ORDER BY 
+  employee_id ASC
